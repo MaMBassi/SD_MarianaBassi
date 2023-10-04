6477fbe31161ceac7c16bbf050be794bb7ee376d
MaMBassi <mariana.bassi@ges.inatel.br>
2023-10-04 17:21:43 -0300

    Aula Lab 01
    Pacote Entity - classes - atributos - getters e setters

---
+++ workspace_eclipse/lab_jpa/src/main/java/br/inatel/labs/labjpa/entity/NotaCompra.java
@@ -0,0 +1,96 @@
+package br.inatel.labs.labjpa.entity;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+
+@Entity
+public class NotaCompra {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotNull
+	@Past
+	private LocalDate dataEmissao;
+	
+	@ManyToOne
+	private Fornecedor fornecedor;
+	
+	@OneToMany(mappedBy = "notaCompra")
+	private List<NotaCompraItem> listaNotaCompraItem;
+	
+	public BigDecimal getCalculoTotalNota() {
+		BigDecimal total = this.listaNotaCompraItem.stream()
+		      .map( i -> i.getCalculoTotalItem() )
+		      .reduce( BigDecimal.ZERO, BigDecimal::add );
+
+		return total;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public LocalDate getDataEmissao() {
+		return dataEmissao;
+	}
+
+	public void setDataEmissao(LocalDate dataEmissao) {
+		this.dataEmissao = dataEmissao;
+	}
+
+	public Fornecedor getFornecedor() {
+		return fornecedor;
+	}
+
+	public void setFornecedor(Fornecedor fornecedor) {
+		this.fornecedor = fornecedor;
+	}
+
+	public List<NotaCompraItem> getListaNotaCompraItem() {
+		return listaNotaCompraItem;
+	}
+
+	public void setListaNotaCompraItem(List<NotaCompraItem> listaNotaCompraItem) {
+		this.listaNotaCompraItem = listaNotaCompraItem;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		NotaCompra other = (NotaCompra) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	@Override
+	public String toString() {
+		return "NotaCompra [id=" + id + ", dataEmissao=" + dataEmissao + "]";
+	}
+
+	
+}
---
+++ workspace_eclipse/lab_jpa/src/test/java/br/inatel/labs/labjpa/LabJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.inatel.labs.labjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
